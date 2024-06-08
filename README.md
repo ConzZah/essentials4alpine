280b4225869a45a27ce8a050c62acc1c2d0f6777
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-08 08:45:59 +0200

    - added: bash-completion, bash-doc and man-pages.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # install_essentials4alpine
+
+## Download:
+
+<p> 
+  <a href="https://github.com/ConzZah/install_essentials4alpine/archive/refs/heads/main.zip">
+    <img alt="Download_Latest" src="https://img.shields.io/badge/download-latest-0688CB.svg">
+  </a>
+</p>
--- install_essentials4alpine_v1.sh
+++ install_essentials4alpine_v1.sh
@@ -1,11 +1,12 @@
   #==========================================
   # Project: INSTALL_ESSENTIALS4ALPINE_v1.sh
   # Author:  ConzZah / ©️ 2024
-  # Last Modification: 06.06.2024 / 12:42
+  # Last Modification: 08.06.2024 / 08:38
   #==========================================
 clear
 echo "NOTE! PLEASE ENABLE COMMUNITY REPOS FOR THIS TO WORK."; echo ""; echo ""
 echo "This will install the following packages:"; echo ""
+echo "- man"
 echo "- lsblk"
 echo "- mount"
 echo "- nano"
@@ -16,10 +17,10 @@ echo "- curl"
 echo "- git"
 echo "- openssh"
 echo "- sudo"
-echo "- bash"
+echo "- bash, bash-completion, bash-doc"
 echo "- python3"
 echo ""
 echo "[ PRESS ANY KEY TO START ]"; read -n 1 -s
 doas apk -U upgrade # <-- runs apk update & upgrade before installing anything 
-doas apk add lsblk mount nano ntfs-3g 7zip neofetch curl git openssh sudo bash python3
+doas apk add mandoc man-pages lsblk mount nano ntfs-3g 7zip neofetch curl git openssh sudo bash bash-completion bash-doc python3
 #   ^  ^  ^  packages to be installed
