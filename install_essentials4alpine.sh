56e874d9d90e5d909cbdf2737a0e9f82e02b5d76
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-09 14:22:16 +0200

    replaced neofetch with fastfetch

--- install_essentials4alpine.sh
+++ install_essentials4alpine.sh
@@ -1,26 +1,26 @@
   #==========================================
-  # Project: INSTALL_ESSENTIALS4ALPINE_v1.sh
+  # Project: INSTALL_ESSENTIALS4ALPINE.sh
   # Author:  ConzZah / ©️ 2024
-  # Last Modification: 08.06.2024 / 08:38
+  # Last Modification: 09.06.2024 / 14:20
   #==========================================
 clear
 echo "NOTE! PLEASE ENABLE COMMUNITY REPOS FOR THIS TO WORK."; echo ""; echo ""
 echo "This will install the following packages:"; echo ""
-echo "- man"
 echo "- lsblk"
 echo "- mount"
 echo "- nano"
 echo "- ntfs-3g"
 echo "- 7zip"
-echo "- neofetch"
+echo "- fastfetch"
 echo "- curl" 
 echo "- git"
 echo "- openssh"
 echo "- sudo"
+echo "- man-pages, mandoc"
 echo "- bash, bash-completion, bash-doc"
 echo "- python3"
 echo ""
 echo "[ PRESS ANY KEY TO START ]"; read -n 1 -s
 doas apk -U upgrade # <-- runs apk update & upgrade before installing anything 
-doas apk add mandoc man-pages lsblk mount nano ntfs-3g 7zip neofetch curl git openssh sudo bash bash-completion bash-doc python3
+doas apk add lsblk mount nano ntfs-3g 7zip fastfetch curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3
 #   ^  ^  ^  packages to be installed
