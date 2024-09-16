c7938625cd59aa80a717c9ebcab3d4d2c3b44726
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-09-16 18:17:39 +0200

    Update essentials4alpine.sh
    added vlc cuz essential

--- essentials4alpine.sh
+++ essentials4alpine.sh
@@ -21,6 +21,7 @@ echo "- sudo"
 echo "- man-pages, mandoc"
 echo "- bash, bash-completion, bash-doc"
 echo "- python3"
+echo "- vlc-qt"
 echo "- w3m, w3m-image, w3m-doc"
 echo "- xz"
 echo "- shadow"
@@ -38,5 +39,5 @@ doas ufw default allow outgoing # <-- allow all outgoing traffic
 doas ufw limit SSH  # <-- open SSH port but protect against brute-force login attacks
 doas ufw reload # <-- reload ufw when done & continue
 #########################################
-doas apk add lsblk mount nano ntfs-3g 7zip fastfetch ffmpeg curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 w3m w3m-image w3m-doc xz shadow gvfs udisks2
+doas apk add lsblk mount nano ntfs-3g 7zip fastfetch ffmpeg curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 vlc-qt w3m w3m-image w3m-doc xz shadow gvfs udisks2
 #   ^  ^  ^  packages to be installed  ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^
