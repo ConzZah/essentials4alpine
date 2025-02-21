ba6ac0c89f8b2fe54145a6bd5b3d454173bccf90
ConzZah <69615452+ConzZah@users.noreply.github.com>
2025-02-21 21:40:53 +0100

    Update essentials4alpine.sh
    added important stuff

--- essentials4alpine.sh
+++ essentials4alpine.sh
@@ -1,12 +1,16 @@
   #==========================================
-  # Project: INSTALL_ESSENTIALS4ALPINE.sh
-  # Author:  ConzZah / ©️ 2024
-  # Last Modification: 27.06.2024 / 16:59
+  # Project: essentials4alpine.sh
+  # Author:  ConzZah / ©️ 2025
+  # Last Modification: 21.02.2025 / 21:40
   #==========================================
 clear
 echo "NOTE! PLEASE ENABLE COMMUNITY REPOS FOR THIS TO WORK."; echo ""; echo ""
 echo "This will install the following packages:"; echo ""
 echo "- ufw + default rules"
+echo "- coreutils"
+echo "- pciutils"
+echo "- grep"
+echo "- sed"
 echo "- lsblk"
 echo "- mount"
 echo "- nano"
@@ -39,5 +43,5 @@ doas ufw default allow outgoing # <-- allow all outgoing traffic
 doas ufw limit SSH  # <-- open SSH port but protect against brute-force login attacks
 doas ufw reload # <-- reload ufw when done & continue
 #########################################
-doas apk add lsblk mount nano ntfs-3g 7zip fastfetch ffmpeg curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 vlc-qt w3m w3m-image w3m-doc xz shadow gvfs udisks2
+doas apk add coreutils pciutils grep sed lsblk mount nano ntfs-3g 7zip fastfetch ffmpeg curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 vlc-qt w3m w3m-image w3m-doc xz shadow gvfs udisks2
 #   ^  ^  ^  packages to be installed  ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^
