35c4933cb6eb32f92fc95ac798bc5ec944746354
ConzZah <69615452+ConzZah@users.noreply.github.com>
2025-02-25 06:47:45 +0100

    Update essentials4alpine.sh
    - removed coreutils because of interferences with other packages

--- essentials4alpine.sh
+++ essentials4alpine.sh
@@ -7,7 +7,6 @@ clear
 echo "NOTE! PLEASE ENABLE COMMUNITY REPOS FOR THIS TO WORK."; echo ""; echo ""
 echo "This will install the following packages:"; echo ""
 echo "- ufw + default rules"
-echo "- coreutils"
 echo "- pciutils"
 echo "- grep"
 echo "- sed"
@@ -43,5 +42,5 @@ doas ufw default allow outgoing # <-- allow all outgoing traffic
 doas ufw limit SSH  # <-- open SSH port but protect against brute-force login attacks
 doas ufw reload # <-- reload ufw when done & continue
 #########################################
-doas apk add coreutils pciutils grep sed lsblk mount nano ntfs-3g 7zip fastfetch ffmpeg curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 vlc-qt w3m w3m-image w3m-doc xz shadow gvfs udisks2
+doas apk add pciutils grep sed lsblk mount nano ntfs-3g 7zip fastfetch ffmpeg curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 vlc-qt w3m w3m-image w3m-doc xz shadow gvfs udisks2
 #   ^  ^  ^  packages to be installed  ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^
